d988c9367c7e8a862d4ad2df8d74f95b9462af7b
gabiborbolato <gabiborbolato@hotmail.com>
2022-01-31 21:38:22 -0300

    Atividade 3:
    - Corrigindo os arquivos de actions e commands
    
    Signed-off-by: gabiborbolato <gabiborbolato@hotmail.com>

--- Aula 4/ui-testing/cypress/support/commands.js
+++ Aula 4/ui-testing/cypress/support/commands.js
@@ -1,9 +1,9 @@
 import loc from  '../support/locators'
 
 Cypress.Commands.add('clickAddSkillButton', (label) => {
-    cy.get(loc.BUTTON).click();
-  });
+  cy.get(loc.BUTTON).click();
+});
   
-  Cypress.Commands.add('visitSite', () => {
-    cy.visit('http://localhost:3000/');
-  });
+Cypress.Commands.add('visitSite', () => {
+  cy.visit('http://localhost:3000/');
+});
